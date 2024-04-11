8d23887444418a8f3a8094a22bbcf6bcd425589e
SELMA_SUGEY_MENDOZA_CASTREJON <a.21308051280386@cbtis128.edu.mx>
2024-04-11 17:56:51 +0000

    Actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:mendoza/BNavigation/bottom_nav.dart';
+import 'package:mendoza/BNavigation/routes.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return const MaterialApp(
+        title: "Mi Diseño Mendoza",
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  BNavigator? myBNB;
+
+  @override
+  void initState() {
+    myBNB = BNavigator(currentIndex: (i) {
+      setState(() {
+        index = i;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        title: Text(
+          "Mendoza Selma 0836",
+          style: TextStyle(color: Colors.white),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.pink,
+            Colors.blue,
+          ])),
         ),
       ),
+      bottomNavigationBar: myBNB,
+      body: Routes(index: index),
     );
   }
 }
